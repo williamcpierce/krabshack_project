1e38f201f97b01d9808960d5bf27bdc0702eb220
William Pierce <wpierce118@gmail.com>
2019-10-06 22:55:00 -0400

    Fixed DED LP rate

--- site_app/views.py
+++ site_app/views.py
@@ -24,7 +24,7 @@ def buyback(request):
     instructions = SiteContent.objects.get(field_id='Buyback Instructions')
     guristas_lp_rate = LPRate.objects.get(lp_type='Guristas').lp_rate
     sanshas_lp_rate = LPRate.objects.get(lp_type='Sanshas').lp_rate
-    ded_lp_rate = LPRate.objects.get(lp_type='Sanshas').lp_rate
+    ded_lp_rate = LPRate.objects.get(lp_type='DED').lp_rate
 
     return render(
         request,
