738b2d0facad3559e3900b808054edc31874661d
William Pierce <wpierce118@gmail.com>
2019-10-10 01:17:21 -0400

    Template/js updates

--- static/assets/js/pages/cashouts.js
+++ static/assets/js/pages/cashouts.js
@@ -3,37 +3,36 @@ var KTDatatablesAdvancedFooterCalllback = function() {
 
 	var initTable1 = function() {
 		var table = $('#kt_table_1');
-
 		// begin first table
 		table.DataTable({
-			responsive: true,
-			pageLength: 10,
-			lengthMenu: [[5, 10, 25, 100, -1], [5, 10, 25, 100, 'All']],
+		    scrollResize: true,
+		    scrollX: true,
+		    scrollY: 100,
+		    scrollCollapse: true,
+		    paging: false,
+		    info: false,
 			footerCallback: function(row, data, start, end, display) {
 
-				var column = 5;
-				var api = this.api(), data;
+				var api = this.api();
+				var i, column, columnArray = [ 2, 4 ], len = columnArray.length;
+				for (i=0; i<len; ++i) {
+					
+					column = columnArray[i];
 
-				// Remove the formatting to get integer data for summation
-				var intVal = function(i) {
-					return typeof i === 'string' ? i.replace(/[\$,]/g, '') * 1 : typeof i === 'number' ? i : 0;
+					// Remove the formatting to get integer data for summation
+					var intVal = function(val) {
+						return typeof val === 'string' ? val.replace(/[\$,]/g, '') * 1 : typeof val === 'number' ? val : 0;
+					};
+
+				    $( api.column( column ).footer() ).html(
+				        "<div align='right'>" +
+				        api.column( column ).data().reduce( function ( a, b ) {
+				            return KTUtil.numberString((intVal(a) + intVal(b)).toFixed(0));
+				        }, 0 ) +
+				        "</div>"
+				    );
 				};
-
-				// Total over all pages
-				var total = api.column(column).data().reduce(function(a, b) {
-					return intVal(a) + intVal(b);
-				}, 0);
-
-				// Total over this page
-				var pageTotal = api.column(column, {page: 'current'}).data().reduce(function(a, b) {
-					return intVal(a) + intVal(b);
-				}, 0);
-
-				// Update footer
-				$(api.column(column).footer()).html(
-					KTUtil.numberString(pageTotal.toFixed(0)) + '<br/> ( ' + KTUtil.numberString(total.toFixed(0)) + ' total )',
-				);
-			},
+			}
 		});
 	};
 
@@ -50,4 +49,8 @@ var KTDatatablesAdvancedFooterCalllback = function() {
 
 jQuery(document).ready(function() {
 	KTDatatablesAdvancedFooterCalllback.init();
+});
+
+$('#filterField').keyup(function(){
+      $('#kt_table_1').DataTable().search($(this).val()).draw() ;
 });
--- static/assets/js/pages/market.js
+++ static/assets/js/pages/market.js
@@ -3,12 +3,14 @@ var KTDatatablesAdvancedFooterCalllback = function() {
 
 	var initTable1 = function() {
 		var table = $('#kt_table_1');
-
 		// begin first table
 		table.DataTable({
-			responsive: true,
-			pageLength: 10,
-			lengthMenu: [[5, 10, 25, 100, -1], [5, 10, 25, 100, 'All']],
+		    scrollResize: true,
+		    scrollX: true,
+		    scrollY: 100,
+		    scrollCollapse: true,
+		    paging: false,
+		    info: false,
 		});
 	};
 
@@ -25,4 +27,8 @@ var KTDatatablesAdvancedFooterCalllback = function() {
 
 jQuery(document).ready(function() {
 	KTDatatablesAdvancedFooterCalllback.init();
+});
+
+$('#filterField').keyup(function(){
+      $('#kt_table_1').DataTable().search($(this).val()).draw() ;
 });
