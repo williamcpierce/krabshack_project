0e4f84afd1410eacb75e747f9c70514d5dccf777
William Pierce <wpierce118@gmail.com>
2020-02-08 13:12:25 -0500

    Config key

--- krabshack_project/settings.py
+++ krabshack_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config['DJANGO_SECRET_KEY']
+DEBUG = config['DEBUG']
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -150,8 +150,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
 if DEBUG:
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
@@ -159,6 +157,7 @@ if DEBUG:
     ]
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 
 # Authentication backends
