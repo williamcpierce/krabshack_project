31d18a442e2fab260a7476c280cc3696050cfd5b
William Pierce <wpierce118@gmail.com>
2020-07-11 19:16:43 -0400

    Added home path

--- src/site_app/urls.py
+++ src/site_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import buyback, couriers
+from .views import buyback, couriers, home
 
 
 urlpatterns = [
+    path('', home, name='home'),
     path('services/buyback/', buyback, name='buyback'),
     path('services/couriers/', couriers, name='couriers')
 ]
