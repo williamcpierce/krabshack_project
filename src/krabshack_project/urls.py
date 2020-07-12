0963a6867729772c7455c9829f8e6ed59bb9bc6b
William Pierce <wpierce118@gmail.com>
2020-07-11 21:55:01 -0400

    OAuth URL

--- src/krabshack_project/urls.py
+++ src/krabshack_project/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
@@ -11,8 +12,8 @@ urlpatterns = [
     path('fit/', include('fit_app.urls'), name='fit'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('oauth/', include('social_django.urls', namespace='social'), name='oauth'),
-    path('services/', include('site_app.urls'), name='site')
+    path('services/', include('site_app.urls'), name='site'),
+    url(r'^oauth/', include('social_django.urls', namespace='social'))
 ]
 
 
